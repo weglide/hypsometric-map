a1eb2409581f2c4552da0cdf0092e7010f45a14d
Samuel Leihkamm <s.leihkamm@gmx.com>
2020-07-05 15:41:23 +0200

    [COLORS] fine tuning

--- src/converter.py
+++ src/converter.py
@@ -69,8 +69,8 @@ class Color:
         [205, 159, 67],
         [183, 108, 93],
         [150, 110, 150],
-        [155, 155, 155],
-        [233, 233, 233],
+        [185, 185, 185],
+        [246, 246, 246],
         [144, 224, 255],
     ])
 
@@ -79,12 +79,12 @@ class Color:
         -40,    # Light Blue
         0,      # Dark Green
         170,    # Medium Green 
-        350,    # Light Green
-        720,    # Light Yellow
-        1100,   # Gold
-        1900,   # Red
-        2400,   # Violet
-        3000,   # Grey
+        400,    # Light Green
+        800,    # Light Yellow
+        1380,   # Gold
+        1800,   # Red
+        2300,   # Violet
+        2900,   # Grey
         3800,   # White 
         6800    # Ice Blue
     ])
