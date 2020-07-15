25839049eb764b448f9d6ddcc0eb9b615255dffe
Samuel Leihkamm <s.leihkamm@gmx.com>
2020-07-15 16:12:34 +0200

    [COLORS] light/intense color scheme

--- src/converter.py
+++ src/converter.py
@@ -21,8 +21,8 @@ futures = []
 
 
 class Color:
-    dirname = Path('planet/terrarium') # path to source files
-    hd_dirname = Path('planet/hd_terrarium') # path to hd source files
+    dirname = Path('data/terrarium') # path to source files
+    hd_dirname = Path('data/hd_terrarium') # path to hd source files
 
     foldername: str = 'hypsometric' # foldername to replace "terrarium" in output
     hd_foldername: str = 'hd_terrarium' # foldername to replace hd data
@@ -33,33 +33,7 @@ class Color:
 
     meters_per_degree = equator_radius * np.pi / 180
 
-    land_colors_older = np.array([
-        [0, 0, 81],
-        [0, 174, 162],
-        [59, 123, 65],
-        [117, 169, 97],
-        [231, 226, 162],
-        [205, 159, 67],
-        [183, 108, 93],
-        [165, 120, 165],
-        [200, 200, 200],
-        [255, 255, 255],
-        [171, 231, 255],
-    ])
     land_colors_old = np.array([
-        [0, 0, 81],
-        [0, 174, 162],
-        [59, 123, 65],
-        [102, 170, 75],
-        [238, 233, 158],
-        [205, 159, 67],
-        [183, 108, 93],
-        [149, 108, 149],
-        [204, 204, 204],
-        [255, 255, 255],
-        [171, 231, 255],
-    ])
-    land_colors = np.array([
         [0, 0, 81],
         [0, 174, 162],
         [59, 107, 65],
@@ -74,17 +48,35 @@ class Color:
         [144, 224, 255],
     ])
 
+    land_colors = np.array([
+        [0, 0, 81],
+        [0, 174, 162],
+        [107, 185, 100], # dark green
+        [158, 200, 62], # green
+        [250, 219, 84], # mustard
+        [255, 172, 64], # orange
+        [225, 142, 97], # brown
+        [255, 108, 79],  # red
+        [255, 132, 169], # pink
+        [170, 128, 203], # purple
+        [187, 180, 203], # gray
+        [239, 143, 244], # white
+        [123, 218, 255], # blue
+    ])
+
+
     land_stops = np.array([ # Stops blend from current to next value
         -8000,  # Dark Blue
         -40,    # Light Blue
-        0,      # Dark Green
-        150,    # Medium Green 
-        400,    # Light Green
-        800,    # Light Yellow
-        1350,   # Gold
-        1800,   # Red
-        2300,   # Violet
-        3000,   # Grey
+        0,      # Emerald
+        180,    # Neon Green 
+        380,    # Lemon Yellow
+        700,    # Orange
+        1000,   # Brown
+        1350,   # red
+        1800,   # Pink
+        2300,   # Purple
+        3000,   # Gray
         4100,   # White 
         6600    # Ice Blue
     ])
